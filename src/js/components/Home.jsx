690fd7fa348cf58733f92a1aefa207f69f54a853
John Samper <johnsamperse@gmail.com>
2025-05-30 05:43:22 +0000

    commit 1 thats all for toninight, my eyes hurt :) idk im im writing code or hieroglyphics

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,29 @@
-import React from "react";
+import React , {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+// this will set the color of the traffic light based off of how it is clicked :) 
+const [selectedColor, setSelectedColor] = React.useState("red");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	return (
+
+		<div className="top pole"> yo im a pole yo :)
+			<div className="traffic-light">
+				<div onClick={ () => setSelectedColor("red")}
+				 className={"light red" +((selectedColor === "red") ? " glow " : "")}>
+				</div>
+				<div onClick={ () => setSelectedColor("yellow")}
+				 className={"light yellow " + ((selectedColor === "yellow") ? " glow " : "")}>
+				</div>
+				<div onClick={ () => setSelectedColor("green")}
+				 className={"light green " + ((selectedColor === "green") ? " glow " : "")}> 
+				</div>
+			</div>
 		</div>
+	
 	);
 };
 
